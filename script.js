a2b4322f4bb69f3d0514f44428e7201839057617
user.Powerpoffgirl <email.jyotisingh13@gmail.com>
2023-01-12 12:29:07 +0530

    completed

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your code here
 
+const btn = document.getElementById("swap");
+const app = document.getElementById("app");
+
+btn.addEventListener("click", changeColor);
+
+function changeColor() {
+  if (app.style.backgroundColor == "black") {
+    app.style.backgroundColor = "white";
+  } else {
+    app.style.backgroundColor = "black";
+  }
+}
